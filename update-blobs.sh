428237004f468acffa41634bc76c2efdc87f010b
Pavel Korolev <dev@borodust.org>
2018-10-08 22:47:27 +0300

    Update to v1.0.2

--- update-blobs.sh
+++ update-blobs.sh
@@ -22,15 +22,15 @@ for arch in "${!archs[@]}" ; do
         target_dir="$script_dir/${archs[$arch]}/"
         mkdir -p "$target_dir"
 
-        lib="lib$name.${platforms[$platform]}.bodged"
+        lib="lib$name.${platforms[$platform]}"
         url="$repository_url/releases/download/$1/$lib-$arch-$platform-$1"
         echo "Downloading $lib from $url to $target_dir/$lib"
         if ! wget -O "$target_dir/$lib" -q "$url" ; then
             echo "$lib download failed"
             exit 1;
         fi
-        wrapper="$lib.wrapper"
-        wrapper_url="$repository_url/releases/download/$1/$lib.wrapper-$arch-$platform-$1"
+        wrapper="lib$name.wrapper.${platforms[$platform]}"
+        wrapper_url="$repository_url/releases/download/$1/$wrapper-$arch-$platform-$1"
         echo "Downloading $wrapper from $url to $target_dir/$wrapper"
         if ! wget -O "$target_dir/$wrapper" -q "$wrapper_url" ; then
             echo "$wrapper download failed"
